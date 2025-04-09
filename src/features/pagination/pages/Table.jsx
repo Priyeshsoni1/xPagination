057b115617e18a03fbd537caf5039afd9f16eac1
Priyesh Soni <priyesh9792@gmail.com>
2025-04-09 08:45:05 +0530

    Pagination implemented

---
+++ src/features/pagination/pages/Table.jsx
@@ -0,0 +1,128 @@
+import React, { useEffect, useState } from "react";
+import { fetchTable } from "../server/fetcher";
+
+export const Table = () => {
+  const [tableData, setTableData] = useState([]);
+  const [showData, setShowData] = useState([]);
+  const [count, setCount] = useState(1);
+  const fetchData = async () => {
+    const data = await fetchTable();
+    setTableData(data);
+    setShowData(data.slice(0, 10 * count));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  console.log(tableData);
+
+  const handleNext = () => {
+    if (count < Math.ceil(tableData.length / 10)) {
+      setShowData(tableData.slice(10 * count, 10 * (count + 1)));
+      setCount((prev) => prev + 1);
+    }
+  };
+  const handlePrevious = () => {
+    if (count > 1) {
+      setShowData(tableData.slice(10 * (count - 1), 10 * count));
+      setCount((prev) => prev - 1);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Employee Data Table</h2>
+      <table border="1" style={{ width: "100%", borderCollapse: "collapse" }}>
+        <thead
+          style={{
+            padding: "1rem",
+            height: "2rem",
+            backgroundColor: "#27A34DFF",
+            color: "#FFFFFFFF",
+          }}
+        >
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Role</th>
+          </tr>
+        </thead>
+        <tbody>
+          {showData.length > 0 ? (
+            showData.map((item) => (
+              <tr
+                key={item.id}
+                style={{
+                  padding: "1rem",
+                  height: "2rem",
+                }}
+              >
+                <td>{item.id}</td>
+                <td>{item.name}</td>
+                <td>{item.email}</td>
+                <td>{item.role}</td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan="4" style={{ textAlign: "center" }}>
+                No Data Available
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+      <div
+        style={{
+          marginTop: "2rem",
+          display: "flex",
+          justifyContent: "center",
+          alignContent: "center",
+          alignSelf: "center",
+          textAlign: "center",
+          alignItems: "center",
+          gap: "2rem",
+        }}
+      >
+        <button
+          onClick={handlePrevious}
+          style={{
+            backgroundColor: "#27A34DFF",
+            color: "#FFFFFFFF",
+            height: "1.7rem",
+            borderRadius: "5px",
+          }}
+        >
+          Previous
+        </button>
+        <p
+          style={{
+            backgroundColor: "#27A34DFF",
+            color: "#FFFFFFFF",
+            height: "2rem",
+            width: "2rem",
+            borderRadius: "5px",
+
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          {count}
+        </p>
+        <button
+          onClick={handleNext}
+          style={{
+            backgroundColor: "#27A34DFF",
+            color: "#FFFFFFFF",
+            height: "1.7rem",
+            borderRadius: "5px",
+          }}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
---
+++ src/features/pagination/server/Fetcher.js
@@ -0,0 +1,17 @@
+export const fetchTable = async () => {
+  try {
+    const res = await fetch(
+      "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json"
+    );
+    if (!res.ok) {
+      throw new Error(`HTTP Error: ${res.status} ${res.statusText}`);
+    }
+    const data = await res.json();
+    if (!Array.isArray(data)) {
+      throw new Error("API response is not an array");
+    }
+    return data || [];
+  } catch (err) {
+    console.log("Error in Data Fetching:", err);
+  }
+};
